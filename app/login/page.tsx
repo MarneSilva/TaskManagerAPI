19025eec6372a2458ee1e37f27ae992f7c7b7a7b
Marne Silva <103045115+MarneSilva@users.noreply.github.com>
2024-05-20 07:32:12 -0300

    Update page.tsx
    Ajustes finais antes do começo da aula.

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form"
 import { useContext } from "react"
 const Login = async ({}) => {
     const {register, handleSubmit} = useForm<SignIdData>()
-    const { login } = useContext(AuthContext)
+    const { login, resultado } = useContext(AuthContext)
     const handleLogin = async (data : SignIdData) => { 
         await login(data)
     }
@@ -30,6 +30,7 @@ const Login = async ({}) => {
                 />
                 <input type="submit" value="Acessar" />
             </form>
+            <p>{resultado}</p>
         </div>
     )
 }
